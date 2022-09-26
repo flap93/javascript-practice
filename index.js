9fb1e7749d0c1b46fad409a2f0e803d9bca305d8
FELIX A PALACIOS <71362706+flap93@users.noreply.github.com>
2022-09-26 22:44:08 +0000

    finish with array destructuring

--- index.js
+++ index.js
@@ -1,84 +1,63 @@
 
+// DESTRUCTURING ARRAYS
 
-// const product = {
-//   name: "Gummy Bears",
-//   inStock: true,
-//   price: 1.99,
-//   flavors: ["grape", "apple", "cherry"]
-// }
-
-// console.log(product)
-
-// console.log(product.inStock);
-
-// const restaurant = {
-//   name: 'Ichiran Ramen',
-//   address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
-//   city: 'Brooklyn',
-//   state: 'NY',
-//   zipcode: '11206',
-// }
-
-// // bracket notation
-// let fullAddress = restaurant["address"] + ", " + restaurant["city"] + ", " +
-//   restaurant["state"] + " " + restaurant["zipcode"];
-
-// console.log(fullAddress)
-// // Dot notation
-
-// // const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`
-
-// // modifying objects 
-
-// const midterms = { danielle: 96, thomas: 78 }
-
-// // updating
-// midterms.thomas = 85
-// // changing
-// midterms["Antonio"] = 95
-
-// console.log(midterms);
-
-
-// // arrays inside of objects
-
-// const comments = [
-//   {username: 'tammy', text: "shdhsdfhs", vote: 9},
-//   {username: 'jessica', text: "hola", vote: 7},
-
-// ]
-
-// console.log(comments[1].text)
-// console.log()
-
-// --------------------------------------------------------
-
-
-const dolphins = (a, b, c) => {
-  return a + b + c / 3;
-}
-
-dolphins(44, 23, 71)
-
-const koala = (a, b, c) => {
-  return a + b + c / 3;
-}
-
-
-koala(65, 54, 49)
-
-
-const checkWinner = (dolphins , koalas) => {
-  if (dolphins >= 2 * koalas) {
-    console.log(`Dolphins win (${dolphins} vs ${koalas})`);
-  }
-  else if ( koalas >= 2 * dolphins) {
-    console.log(`Koalas win (${koalas} vs ${dolphins})`);
-  }
-  else {
-    console.log("No teams win ")
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function(startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]]
   }
 }
 
+// console.log(restaurant)
 
-checkWinner(dolphins, koala);
+
+// let [one , two] = restaurant.categories
+
+
+
+// console.log(one, two)
+
+// you can skip one element simply by adding a space with spaces
+
+// const [one , ,two] = restaurant.categories
+
+// the ouput is : Italian , vegetarian 
+
+
+// console.log(`The type of restaurant is ${one}`)
+
+//-----------------------------------------------------------------
+// destructuring through a function ---------------------------
+//------------------------------------------------
+console.log(restaurant.order(2, 0)); // testing the data that we are receiving 
+//  we process to destructure 
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+
+console.log(starter, mainCourse)
+
+//---------------------------------------------
+// Nester Array---------------------------
+//-----------------------------------------------------
+
+const nested = [1, 2, 3, [4, 5, 6]]
+
+
+const [a, , , [b, c, d]] = nested
+
+console.log(a, b, c, d);
+
+//-----------------------------------------
+// Default values ---------------------
+//------------------------------------------------------
+
+const [p = 1, q = 1, r = 1] = [8, 9]
+console.log(p, q, r) // basically what this doues get in us temples for our data that we want , so in this case 
+//  we are printig 3 values but since the thrid value of our array its not include the out will be this
+// output :  8 , 9 , 1
+// but if add a third number in the array we will get the numbers 
+// example:  = [8, 9, 10]  output :  8, 9, 10
